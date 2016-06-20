b6ca80c9e66d70299689a58c96eaf1f4cb2c0b90
Willem_Pijl <willemvanderpijl@hotmail.com>
2016-06-20 11:53:23 +0200

    Initial commit

---
+++ app/src/main/java/topkek/dev4/Hoofdmenu.java
@@ -0,0 +1,13 @@
+package topkek.dev4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hoofdmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoofdmenu);
+    }
+}
